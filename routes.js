30c196a4d08e44272094b360437802839f9d3e90
lestercoyoy <lester_cch@hotmail.com>
2022-10-12 16:15:49 -0600

    ADD
    Get method with SQL

--- routes.js
+++ routes.js
@@ -2,7 +2,17 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req,res)=>{
-    res.send('testing api')
+    req.getConnection((err,conn)=>{
+        if(err)
+            return res.send(err)
+            
+        conn.query('SELECT * FROM books', (err,rows)=>{
+            if(err)
+                return res.send(err)
+
+            res.json(rows)
+        })
+    })
 })
 
 module.exports = routes
