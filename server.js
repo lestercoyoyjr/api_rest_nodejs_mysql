d6fa820be2d97692eeb9288243b4f726dd0b727f
lestercoyoy <lester_cch@hotmail.com>
2022-10-11 20:31:52 -0600

    ADD
    Api testing method

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const myconn = require('express-myconnection')
 
 const app = express()
 app.set('port', process.env.PORT || 9000)
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'library'
+}
 
-// middleware --------------------------------------------------
+
+// middlewares --------------------------------------------------
+app.use(myconn(mysql,dbOptions,'single'))
 
 // routes ------------------------------------------------------
 app.get('/', (req,res)=>{
     res.send('Welcome to my API')
+})
+app.get('/api', (req,res)=>{
+    res.send('testing api')
 })
 
 // Server running -----------------------------------------------
