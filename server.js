77717216fe10c6c407b15cc58e8a9638892f56c4
lestercoyoy <lester_cch@hotmail.com>
2022-10-11 19:56:27 -0600

    ADD
    npm nodemon and mysql connection

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
 
 const app = express()
 app.set('port', process.env.PORT || 9000)
 
+// middleware --------------------------------------------------
+
+// routes ------------------------------------------------------
+app.get('/', (req,res)=>{
+    res.send('Welcome to my API')
+})
+
+// Server running -----------------------------------------------
 app.listen(app.get('port'), ()=>{
     console.log('server running on port ', app.get('port'))
 })
